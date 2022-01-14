36995689aeb92fa308de02423d13391f5184b038
amongusofficial <95932797+amongusofficial@users.noreply.github.com>
2022-01-14 13:01:39 -0500

    Delete version_config.py

--- version_config.py
+++
@@ -1,7 +0,0 @@
-from settings import program_version
-from directory import path
-import os
-os.chdir(path)
-file=open('version.py','w')
-file.write('setup_version = \''+program_version+"'")
-file.close()
